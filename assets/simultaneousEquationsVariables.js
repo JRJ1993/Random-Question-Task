8eb03b3db5cc19807b586f05a52e0afdbe3aa52c
James Jordan <james.r.jordan1993@hotmail.com>
2023-05-19 14:35:52 +0100

    adds assets for questions

---
+++ assets/simultaneousEquationsVariables.js
@@ -0,0 +1,15 @@
+const simultaneousEquationsVariables = [
+    { name: 'potatoes', unit: 'kg', coefficient: 0.6 },
+    { name: 'carrots', unit: 'kg', coefficient: 0.5 },
+    { name: 'cars', unit: '', coefficient: 10000 },
+    { name: 'TV', unit: '', coefficient: 500 },
+    { name: 'footballs', unit: '', coefficient: 15 },
+    { name: 'basketballs', unit: '', coefficient: 12 },
+    { name: 'rugby balls', unit: '', coefficient: 10 },
+    { name: 'pumpkin', unit: 'kg', coefficient: 2 },
+    { name: 'rice', unit: 'kg', coefficient: 1.5 },
+    { name: 'grapes', unit: 'kg', coefficient: 3 },
+    { name: 'microwaves', unit: '', coefficient: 75 },
+];
+
+export default simultaneousEquationsVariables;
