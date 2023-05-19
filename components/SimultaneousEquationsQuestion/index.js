7da6ef188c07736865e80a2e0823bc2a14ec12b0
James Jordan <james.r.jordan1993@hotmail.com>
2023-05-19 14:38:07 +0100

    creates a question component

---
+++ components/SimultaneousEquationsQuestion/index.js
@@ -0,0 +1,10 @@
+import StaticMath from '../StaticMath/StaticMath'
+
+const SimultaneousEquationsQuestion = () => {
+    return (
+        <StaticMath latex={`\\text{Generate a random question and display it here.}`} />
+    )
+}
+
+export default SimultaneousEquationsQuestion
+
