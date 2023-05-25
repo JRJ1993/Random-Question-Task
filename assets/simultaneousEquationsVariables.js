637cdfbff9afd4e1130a25baaaa80b65844218b1
James Jordan <james.r.jordan1993@hotmail.com>
2023-05-25 16:57:00 +0100

    changes the asset library

--- assets/simultaneousEquationsVariables.js
+++ assets/simultaneousEquationsVariables.js
@@ -1,15 +1,15 @@
 const simultaneousEquationsVariables = [
-    { name: 'potatoes', unit: 'kg', coefficient: 0.6 },
-    { name: 'carrots', unit: 'kg', coefficient: 0.5 },
-    { name: 'cars', unit: '', coefficient: 1000 },
-    { name: 'TVs', unit: '', coefficient: 50 },
-    { name: 'footballs', unit: '', coefficient: 15 },
-    { name: 'basketballs', unit: '', coefficient: 12 },
-    { name: 'rugby balls', unit: '', coefficient: 10 },
-    { name: 'pumpkin', unit: 'kg', coefficient: 2 },
-    { name: 'rice', unit: 'kg', coefficient: 1.5 },
-    { name: 'grapes', unit: 'kg', coefficient: 3 },
-    { name: 'microwaves', unit: '', coefficient: 75 },
+    { name: 'potatoes', unit: 'kg', coefficient: 0.06 },
+    { name: 'carrots', unit: 'kg', coefficient: 0.05 },
+    { name: 'cars', unit: '', coefficient: 500 },
+    { name: 'TVs', unit: '', coefficient: 10 },
+    { name: 'footballs', unit: '', coefficient: 1 },
+    { name: 'basketballs', unit: '', coefficient: 1 },
+    { name: 'rugby balls', unit: '', coefficient: 1 },
+    { name: 'pumpkin', unit: 'kg', coefficient: 0.2 },
+    { name: 'rice', unit: 'kg', coefficient: 0.15 },
+    { name: 'grapes', unit: 'kg', coefficient: 0.3 },
+    { name: 'microwaves', unit: '', coefficient: 10 },
 ];
 
 export default simultaneousEquationsVariables;
