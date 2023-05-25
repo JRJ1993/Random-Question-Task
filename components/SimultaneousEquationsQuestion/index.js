082c48c38c836b9342d854f8317959189aeeb350
James Jordan <james.r.jordan1993@hotmail.com>
2023-05-25 16:57:37 +0100

    remove console logs

--- components/SimultaneousEquationsQuestion/index.js
+++ components/SimultaneousEquationsQuestion/index.js
@@ -7,19 +7,10 @@ const SimultaneousEquationsQuestion = ({ variableOne,
     equationOneTotal,
     equationTwoTotal }) => {
 
-    console.log(variableOne,
-        variableTwo,
-        equationOneRandomCoefficient,
-        equationTwoRandomCoefficient,
-        equationOneTotal,
-        equationTwoTotal)
-
-    // console.log(variableOne, variableTwo, equationOneRandomCoefficient, equationTwoRandomCoefficient, equationOneTotal, equationTwoTotal)
-
     return (
         <>
-            <StaticMath latex={`\\text{${equationOneRandomCoefficient[0] + 1} ${variableOne.unit ? `${variableOne.unit} of` : ''} ${variableOne.name} and ${equationOneRandomCoefficient[1] + 1} ${variableTwo.unit ? `${variableTwo.unit} of` : ''} ${variableTwo.name} have a total cost of £${equationOneTotal}}`} />
-            <StaticMath latex={`\\text{${equationTwoRandomCoefficient[0] + 1} ${variableOne.unit ? `${variableOne.unit} of` : ''} ${variableOne.name} and ${equationTwoRandomCoefficient[1] + 1} ${variableTwo.unit ? `${variableTwo.unit} of` : ''} ${variableTwo.name} have a total cost of £${equationTwoTotal}}`} />
+            <StaticMath latex={`\\text{${equationOneRandomCoefficient[0]} ${variableOne.unit ? `${variableOne.unit} of` : ''} ${variableOne.name} and ${equationOneRandomCoefficient[1]} ${variableTwo.unit ? `${variableTwo.unit} of` : ''} ${variableTwo.name} have a total cost of £${equationOneTotal}}`} />
+            <StaticMath latex={`\\text{${equationTwoRandomCoefficient[0]} ${variableOne.unit ? `${variableOne.unit} of` : ''} ${variableOne.name} and ${equationTwoRandomCoefficient[1]} ${variableTwo.unit ? `${variableTwo.unit} of` : ''} ${variableTwo.name} have a total cost of £${equationTwoTotal}}`} />
         </>
     )
 }
