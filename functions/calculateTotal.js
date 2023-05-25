0251e8e44ccd705b79c0fe217580ca0684a374b7
James Jordan <james.r.jordan1993@hotmail.com>
2023-05-25 16:56:46 +0100

    refactor the total function

--- functions/calculateTotal.js
+++ functions/calculateTotal.js
@@ -1,6 +1,6 @@
 const calculateTotal = (variableOne, variableTwo, equationRandomCoefficient, randomMultiplier) => {
-    const valueOne = (variableOne.coefficient * (equationRandomCoefficient[0] + 1) * randomMultiplier[0])
-    const valueTwo = (variableTwo.coefficient * (equationRandomCoefficient[1] + 1) * randomMultiplier[1])
+    const valueOne = Math.round(variableOne.coefficient * (equationRandomCoefficient[0]) * (randomMultiplier[0]) * 100) / 100
+    const valueTwo = Math.round(variableTwo.coefficient * (equationRandomCoefficient[1]) * (randomMultiplier[1]) * 100) / 100
     return valueOne + valueTwo
 }
 
